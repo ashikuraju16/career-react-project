711dfa4f16fa5961530483e94e22d101ec066cfe
ashikuraju16 <ashikur.r.raju@gmail.com>
2024-05-11 17:30:49 +0600

    body

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,17 @@
 import { NavLink } from "react-router-dom";
-
+import { Icon } from '@iconify/react';
 
 const Footer = () => {
     return (
         <footer className=" p-10 bg-base-200 text-base-content">
         <div className="footer w-[70%] mx-auto">
-        <aside>
-        
+        <aside className="text-4xl flex gap-12 mt-12 ">
+        {/* biome-ignore lint/a11y/useValidAnchor: <explanation> */}
+        <a href=""><Icon className="transition-all hover:rounded-bl-full hover:rounded-tr-full  hover:bg-fuchsia-700 hover:scale-150 duration-1000 hover:cursor-pointer" icon="logos:facebook" /></a>
+        {/* biome-ignore lint/a11y/useValidAnchor: <explanation> */}
+        <a href=""><Icon className="transition-all hover:rounded-bl-full hover:rounded-tr-full  hover:bg-fuchsia-700 hover:scale-150 duration-1000 hover:cursor-pointer" icon="grommet-icons:github" /></a>
+        {/* biome-ignore lint/a11y/useValidAnchor: <explanation> */}
+        <a href=""><Icon className="transition-all hover:rounded-bl-full hover:rounded-tr-full  hover:bg-fuchsia-700 hover:scale-150 duration-1000 hover:cursor-pointer bg-green-700  p-1 rounded-3xl  text-white" icon="jam:fiverr" /></a>
     
       </aside> 
       <nav>
