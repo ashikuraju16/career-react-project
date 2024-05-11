e2c4a9a6d9e3d30645fa5b3cbcf6e41cc2904e0e
ashikuraju16 <ashikur.r.raju@gmail.com>
2024-05-11 17:33:26 +0600

    body

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,37 +1,46 @@
 import { NavLink } from "react-router-dom";
-import { Icon } from '@iconify/react';
+import { Icon } from "@iconify/react";
 
 const Footer = () => {
-    return (
-        <footer className=" p-10 bg-base-200 text-base-content">
-        <div className="footer w-[70%] mx-auto">
+  return (
+    <footer className=" p-10 bg-base-200 text-base-content">
+      <div className="footer w-[70%] mx-auto">
         <aside className="text-4xl flex gap-12 mt-12 ">
-        {/* biome-ignore lint/a11y/useValidAnchor: <explanation> */}
-        <a href=""><Icon className="transition-all hover:rounded-bl-full hover:rounded-tr-full  hover:bg-fuchsia-700 hover:scale-150 duration-1000 hover:cursor-pointer" icon="logos:facebook" /></a>
-        {/* biome-ignore lint/a11y/useValidAnchor: <explanation> */}
-        <a href=""><Icon className="transition-all hover:rounded-bl-full hover:rounded-tr-full  hover:bg-fuchsia-700 hover:scale-150 duration-1000 hover:cursor-pointer" icon="grommet-icons:github" /></a>
-        {/* biome-ignore lint/a11y/useValidAnchor: <explanation> */}
-        <a href=""><Icon className="transition-all hover:rounded-bl-full hover:rounded-tr-full  hover:bg-fuchsia-700 hover:scale-150 duration-1000 hover:cursor-pointer bg-green-700  p-1 rounded-3xl  text-white" icon="jam:fiverr" /></a>
-    
-      </aside> 
-      <nav>
-        <h6 className="footer-title">Services</h6> 
-        <NavLink className="link link-hover">Branding</NavLink>
-        <NavLink className="link link-hover">Design</NavLink>
-        <NavLink className="link link-hover">Marketing</NavLink>
-        <NavLink className="link link-hover">Advertisement</NavLink>
-      </nav> 
-      <nav>
-        <h6 className="footer-title">Company</h6> 
-        <NavLink className="link link-hover">About us</NavLink>
-        <NavLink className="link link-hover">Contact</NavLink>
-        <NavLink className="link link-hover">Jobs</NavLink>
-   
-      </nav> 
-        </div>
-        
-      </footer>
-    );
+          <a href="https://www.facebook.com/ashikuraju16">
+            <Icon
+              className="transition-all hover:rounded-bl-full hover:rounded-tr-full  hover:bg-fuchsia-700 hover:scale-150 duration-1000 hover:cursor-pointer"
+              icon="logos:facebook"
+            />
+          </a>
+          <a href=" https://github.com/ashikuraju16">
+            <Icon
+              className="transition-all hover:rounded-bl-full hover:rounded-tr-full  hover:bg-fuchsia-700 hover:scale-150 duration-1000 hover:cursor-pointer"
+              icon="grommet-icons:github"
+            />
+          </a>
+          <a href="https://www.fiverr.com/ashik_raju?up_rollout=true">
+            <Icon
+              className="transition-all hover:rounded-bl-full hover:rounded-tr-full  hover:bg-fuchsia-700 hover:scale-150 duration-1000 hover:cursor-pointer bg-green-700  p-1 rounded-3xl  text-white"
+              icon="jam:fiverr"
+            />
+          </a>
+        </aside>
+        <nav>
+          <h6 className="footer-title">Services</h6>
+          <NavLink className="link link-hover">Branding</NavLink>
+          <NavLink className="link link-hover">Design</NavLink>
+          <NavLink className="link link-hover">Marketing</NavLink>
+          <NavLink className="link link-hover">Advertisement</NavLink>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Company</h6>
+          <NavLink className="link link-hover">About us</NavLink>
+          <NavLink className="link link-hover">Contact</NavLink>
+          <NavLink className="link link-hover">Jobs</NavLink>
+        </nav>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
