c1281cc0bc6b53e673e23353b8549d71f5d48c69
ashikuraju16 <ashikur.r.raju@gmail.com>
2024-05-13 18:25:43 +0600

    body

--- src/Components/JobDetails/JobDetails.jsx
+++ src/Components/JobDetails/JobDetails.jsx
@@ -1,20 +1,18 @@
 import { useLoaderData, useParams } from "react-router-dom";
 
-
-
 const JobDetails = () => {
-const jobs =   useLoaderData()
+  const jobs = useLoaderData();
 
   const { jobsId } = useParams();
-  const currentJob =jobs.find((job) => job.id === Number.parseInt(jobsId))
-    return (
-        <div>
-            <h1 className="text-5xl text-black font-extrabold text-center">Job Details </h1>
-       name :{currentJob?.job_title}
-       
-
-        </div>
-    );
+  const currentJob = jobs.find((job) => job.id === Number.parseInt(jobsId));
+  return (
+    <div>
+      <h1 className="text-5xl text-black font-extrabold text-center">
+        Job Details{" "}
+      </h1>
+      name :{currentJob?.job_title}
+    </div>
+  );
 };
 
 export default JobDetails;
