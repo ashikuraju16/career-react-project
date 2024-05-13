7c4c4e0512a675e6409fb11b70670ccf465811e3
ashikuraju16 <ashikur.r.raju@gmail.com>
2024-05-13 19:04:33 +0600

    details

--- src/Components/JobDetails/JobDetails.jsx
+++ src/Components/JobDetails/JobDetails.jsx
@@ -6,11 +6,24 @@ const JobDetails = () => {
   const { jobsId } = useParams();
   const currentJob = jobs.find((job) => job.id === Number.parseInt(jobsId));
   return (
-    <div>
-      <h1 className="text-5xl text-black font-extrabold text-center">
+    <div className="w-[80%] mx-auto">
+      <h1 className="text-5xl text-black font-extrabold text-center pt-24">
         Job Details{" "}
       </h1>
-      name :{currentJob?.job_title}
+      <div className="grid grid-cols-3 pt-24 text-xl leading-10 text-[#757575]">
+        <div className="col-span-2 px-8 space-y-12">
+          <h1>
+            {" "}
+            <span className="font-bold text-black">Job Description :</span>{" "}
+            {currentJob?.job_description}
+          </h1>
+          <h1>
+            <span className="font-bold text-black">Job Responsibility : </span>
+            {currentJob?.job_responsibility}
+          </h1>
+        </div>
+        <div className="col-span-1 bg-[#f4f2ff]" />
+      </div>
     </div>
   );
 };
