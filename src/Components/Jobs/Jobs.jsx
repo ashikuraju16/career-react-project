dd9966565696fb168a14757439b71ea5084d16c5
ashikuraju16 <ashikur.r.raju@gmail.com>
2024-05-13 13:05:09 +0600

    job c

--- src/Components/Jobs/Jobs.jsx
+++ src/Components/Jobs/Jobs.jsx
@@ -11,16 +11,20 @@ const Jobs = ({ job }) => {
     job_type,
     salary,
   } = job;
+
   return (
     <div className="border m-4 p-8 rounded-lg">
       <img className="w-[25%]" src={logo} alt="" />
       <h1 className="text-2xl text-black font-extrabold mt-8">{job_title}</h1>
       <p className="text-[#757575] text-xl my-3">{company_name}</p>
-      <div className="flex space-x-6 bg-clip-text bg-gradient-to-r from-indigo-500 from-15% via-sky-500 via-35% to-emerald-500 to-85% text-lg font-bold">
-        <div className="border-2 border- rounded-lg px-4 py-2 ">
-          {remote_or_onsite}
+      <div className="flex  space-x-6 text-lg  font-bold">
+        <div className="rounded size-fit p-0.5  bg-gradient-to-r from-[#818eff] from-10% via-[#8a82ff] via-30% to-[#9576ff] to-85%  ">
+          <div className="px-4 py-1 rounded bg-white ">{remote_or_onsite}</div>
         </div>
-        <div className="border-2 rounded-lg px-4 py-2 ">{job_type}</div>
+        <div className="rounded size-fit p-0.5  bg-gradient-to-r from-[#818eff] from-10% via-[#8a82ff] via-30% to-[#9576ff] to-85%  ">
+          <div className="px-4 py-1 rounded  bg-white ">{job_type}</div>
+        </div>
+        
       </div>
     </div>
   );
