994d1b0129f78da37bc8b4f0bfd8a26ff65e4753
ashikuraju16 <ashikur.r.raju@gmail.com>
2024-05-13 16:18:57 +0600

    button

--- src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -5,6 +5,7 @@ import { NavLink } from "react-router-dom";
 
 const FeaturedJobs = () => {
   const [jobs, setJobs] = useState([]);
+  const [dataLength, setDataLength] =useState(4);
   useEffect(() => {
     fetch("jobs.json")
       .then((res) => res.json())
@@ -20,13 +21,13 @@ const FeaturedJobs = () => {
         </p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-12max-sm:p-6 rounded-xl mt-8 xl:w-[80%] mx-auto md:w-[90%]">
-        {jobs.map((job) => (
+        {jobs.slice(0, dataLength).map((job) => (
           <Jobs key={job.id} job={job} />
         ))}
       </div>
 
-      <div className="pt-12 pb-20 max-sm:py-6">
-        <NavLink className=" flex justify-center items-center max-sm:w-[50%] sm:w-[30%] md:w-[20%] lg-[10%] xl:w-[15%] max-sm:mt-3 text-xl font-bold mx-auto max-sm:text-base max-sm:p-2 max-md:text-2xl max-md:p-4 md:text-2xl p-4 rounded-xl text-white bg-gradient-to-r from-[#818eff] from-10% via-[#8a82ff] via-30% to-[#9576ff] to-85%">
+      <div className="pt-12 pb-20 max-sm:py-6 {dataLength === jobs.length && 'hidden'}">
+        <NavLink onClick={()=>setDataLength(jobs.length)} className=" flex justify-center items-center max-sm:w-[50%] sm:w-[30%] md:w-[20%] lg-[10%] xl:w-[15%] max-sm:mt-3 text-xl font-bold mx-auto max-sm:text-base max-sm:p-2 max-md:text-2xl max-md:p-4 md:text-2xl p-4 rounded-xl text-white bg-gradient-to-r from-[#818eff] from-10% via-[#8a82ff] via-30% to-[#9576ff] to-85%">
           See All Jobs
         </NavLink>
       </div>
