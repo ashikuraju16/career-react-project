663edb6e84635e12a3a92625f0519ca9c1978715
ashikuraju16 <ashikur.r.raju@gmail.com>
2024-05-15 11:49:43 +0600

    body

--- src/Components/AppliedJobs/jobItems.jsx
+++ src/Components/AppliedJobs/jobItems.jsx
@@ -3,46 +3,42 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { NavLink } from "react-router-dom";
 
-// eslint-disable-next-line react/prop-types
 const JobItems = ({ job }) => {
   return (
-    <div className=" h-full mx-auto md:w-[80%] ">
-      <div className="border-2 border-[#e8e8e8] m-4 p-8 rounded-xl flex justify-between">
-        <div className="flex space-x-12">
-          <div className="bg-[#f4f4f4] p-8  h-48 flex items-center">
-            <img className="  " src={job.logo} alt="" />
+    <div className="mx-auto md:w-[80%] py-6">
+      <div className="border-2 border-[#e8e8e8] m-4 p-8 max-sm:p-5 rounded-xl flex flex-col md:flex-row justify-between">
+        <div className="flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-12">
+          <div className="bg-[#f4f4f4] p-8 max-sm:p-6 md:p-0 md:w-48 h-48 flex items-center justify-center">
+            <img className="h-full max-sm:h-20" src={job.logo} alt="" />
           </div>
-          <div className="my-7 space-y-2">
-            <h1 className="text-2xl text-black font-extrabold text-start">
-              {job.job_title}
-            </h1>
-            <p className="text-[#757575] text-xl text-start">
-              {job.company_name}
-            </p>
-            {/* remote_or_onsite && job_type */}
-            <div className="flex max-sm:text-xs  space-x-6 text-lg  font-bold">
-              <div className=" rounded size-fit p-0.5  bg-gradient-to-r from-[#818eff] from-10% via-[#8a82ff] via-30% to-[#9576ff] to-85%  ">
-                <div className="px-4 py-1 rounded bg-white ">
+          <div className="flex-1 flex flex-col justify-between my-7 space-y-2">
+            <div>
+              <h1 className="text-2xl text-black font-extrabold text-start">
+                {job.job_title}
+              </h1>
+              <p className="text-[#757575] text-xl text-start">
+                {job.company_name}
+              </p>
+            </div>
+            <div className="flex flex-wrap space-x-2 text-lg max-sm:text-xs font-bold">
+              <div className="rounded p-0.5 bg-gradient-to-r from-[#818eff] via-[#8a82ff] to-[#9576ff]">
+                <div className="px-4 py-1 max-sm:py-2 rounded bg-white">
                   {job.remote_or_onsite}
                 </div>
               </div>
-              <div className="rounded size-fit p-0.5  bg-gradient-to-r from-[#818eff] from-10% via-[#8a82ff] via-30% to-[#9576ff] to-85%  ">
-                <div className="px-4 py-1 rounded  bg-white ">
+              <div className="rounded p-0.5 bg-gradient-to-r from-[#818eff] via-[#8a82ff] to-[#9576ff]">
+                <div className="px-4 py-1 max-sm:py-2 rounded bg-white">
                   {job.job_type}
                 </div>
               </div>
             </div>
-            {/* location && salary */}
-            <div className="my-8 text-lg text-[#757575]  lg:flex max-sm:flex-col  ">
-              <div className="flex">
-                <Icon className=" mr-2 mt-0.5" icon="mi:location" />
+            <div className="text-lg text-[#757575]">
+              <div className="flex items-center">
+                <Icon className="mr-2 mt-0.5" icon="mi:location" />
                 <div>{job.location}</div>
               </div>
-              <div className="flex">
-                <Icon
-                  className="lg:ml-8 mr-2 mt-0.5 "
-                  icon="solar:dollar-outline"
-                />
+              <div className="flex items-center">
+                <Icon className="mr-2 mt-0.5" icon="solar:dollar-outline" />
                 <div>{job.salary} (per month)</div>
               </div>
             </div>
@@ -50,8 +46,7 @@ const JobItems = ({ job }) => {
         </div>
 
         {/* View Details */}
-
-        <NavLink className="h-12 text-white p-4 flex items-center rounded-md font-extrabold text-lg my-20 bg-gradient-to-r from-[#818eff] from-10% via-[#8a82ff] via-35% to-[#9576ff] to-85%">
+        <NavLink className="mt-6 md:mt-0 w-full md:w-auto h-12 text-white p-4 flex justify-center items-center rounded-md font-extrabold text-lg bg-gradient-to-r from-[#818eff] via-[#8a82ff] to-[#9576ff]">
           View Details
         </NavLink>
       </div>
