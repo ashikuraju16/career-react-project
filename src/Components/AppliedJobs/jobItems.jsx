0ec8097f1a1ffc38b3f39dadbeb460701ff6a73b
ashikuraju16 <ashikur.r.raju@gmail.com>
2024-05-15 16:13:54 +0600

    body

--- src/Components/AppliedJobs/jobItems.jsx
+++ src/Components/AppliedJobs/jobItems.jsx
@@ -4,20 +4,13 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { NavLink } from "react-router-dom";
 
-
 const JobItems = ({ job }) => {
-   
-    
   return (
     <div className="mx-auto md:w-full lg:w-[85%] py-6">
       <div className="border-2 border-[#e8e8e8] m-4 p-8 md:p-5 max-sm:p-5 rounded-xl flex flex-col md:flex-row justify-between">
         <div className="flex flex-col md:flex-row space-y-6 md:space-y-8 md:space-x-8 ">
           <div className="bg-[#f4f4f4] p-8 rounded-lg max-sm:p-6 md:p-4 lg:p-8  flex items-center justify-center">
-            <img
-              className="  h-20 max-w-52"
-              src={job.logo}
-              alt=""
-            />
+            <img className="  h-20 max-w-52" src={job.logo} alt="" />
           </div>
           <div className=" flex flex-col  my-7 space-y-2 md:space-y-4">
             <div>
@@ -53,9 +46,8 @@ const JobItems = ({ job }) => {
           </div>
         </div>
 
-
         <NavLink
-          to="/jobs/:id" 
+          to={`/jobs/${job.id}`}
           className="lg:my-28 sm:my-16 my-6 w-full md:w-auto h-12 text-white p-4 flex justify-center items-center rounded-md font-extrabold text-lg bg-gradient-to-r from-[#818eff] via-[#8a82ff] to-[#9576ff]"
         >
           View Details
